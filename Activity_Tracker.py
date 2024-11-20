04f54f7fe47f470a6f2f4b488d052f31270c2890
TDS <tds@tcg3pl.com>
2024-11-20 16:12:14 -0500

    Adjusting sidebar width.

--- Activity_Tracker.py
+++ Activity_Tracker.py
@@ -15,7 +15,7 @@ st.markdown(
     """
     <style>
         section[data-testid="stSidebar"] {
-            width: 200px !important; # Set the width to your desired value
+            width: 175px !important; # Set the width to your desired value
         }
     </style>
     """,
