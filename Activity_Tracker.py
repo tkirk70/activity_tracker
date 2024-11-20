4fab5b5ca5e4f81ad9e44d6622e08485bc186d0b
TDS <tds@tcg3pl.com>
2024-11-20 16:11:42 -0500

    Adjusting sidebar width.

--- Activity_Tracker.py
+++ Activity_Tracker.py
@@ -15,7 +15,7 @@ st.markdown(
     """
     <style>
         section[data-testid="stSidebar"] {
-            width: 500px !important; # Set the width to your desired value
+            width: 200px !important; # Set the width to your desired value
         }
     </style>
     """,
--- pages/Page_2.py
+++ pages/Page_2.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
 st.title('Page Two is under construction.')
+
+# adjust sidebar width
+# Inject custom CSS to set the width of the sidebar
+st.markdown(
+    """
+    <style>
+        section[data-testid="stSidebar"] {
+            width: 200px !important; # Set the width to your desired value
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
