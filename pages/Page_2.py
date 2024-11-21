ff85ecfbe49bdb27cdfd78fbe1e175920aecf0e1
TDS <tds@tcg3pl.com>
2024-11-21 08:54:38 -0500

    Commenting out the menu additions on page 2.

--- pages/Page_2.py
+++ pages/Page_2.py
@@ -5,16 +5,16 @@ st.set_page_config(
     page_icon=":construction:",
     layout="centered",
     initial_sidebar_state="auto",
-    menu_items={
-        'Get help': 'https://tcg3pl.sharepoint.com/SitePages/Activity-Tracker.aspx',
-        'Report a bug': "https://tcg3pl.sharepoint.com/",
-        'About': "This app is a collection of warehouse hours broken down by customer and activity"
-    }
+    # menu_items={
+    #     'Get help': 'https://tcg3pl.sharepoint.com/SitePages/Activity-Tracker.aspx',
+    #     'Report a bug': "https://tcg3pl.sharepoint.com/",
+    #     'About': "This app is a collection of warehouse hours broken down by customer and activity"
+    # }
 )
 
 st.title('Page Two is under construction.')
 
-# adjust sidebar width
+# adjust sidebar width - looks like there is a minimum but this worked and the menu can not be expanded or contracted.
 # Inject custom CSS to set the width of the sidebar
 st.markdown(
     """
