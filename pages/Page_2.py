0aa3d7f1da90d735855853e0676b3623c147edab
TDS <tds@tcg3pl.com>
2024-11-21 08:39:28 -0500

    Config Page 2.

--- pages/Page_2.py
+++ pages/Page_2.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Data Construction",
+    page_icon=":construction:",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items={
+        'SharePoint': 'https://tcg3pl.sharepoint.com/SitePages/Activity-Tracker.aspx',
+        'Suggestions': "tds@tcg3pl.com",
+        'About': "This app is a collection of warehouse hours broken down by customer and activity"
+    }
+)
+
 st.title('Page Two is under construction.')
 
 # adjust sidebar width
