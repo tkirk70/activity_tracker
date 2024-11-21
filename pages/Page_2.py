b0a5aa7b38c530c9964b8f4f86229b182f789b8d
TDS <tds@tcg3pl.com>
2024-11-21 13:51:04 -0500

    Adding pie chart.

--- pages/Page_2.py
+++ pages/Page_2.py
@@ -40,13 +40,13 @@ df['Duration'] = df['Duration'].round(2)
 
 st.sidebar.header("Filter By:")
 
-employee_filter = st.sidebar.multiselect("Filter by Employee:",
+employee_filter = st.sidebar.selectbox("Filter by Employee:",
                                   options=df['Employee'].unique(),
                                   default=None)
-customer_filter = st.sidebar.multiselect("Filter by Customer:",
+customer_filter = st.sidebar.selectbox("Filter by Customer:",
                                   options=df['Customer'].unique(),
                                   default=None)
-activity_filter = st.sidebar.multiselect("Filter by Activity:",
+activity_filter = st.sidebar.selectbox("Filter by Activity:",
                                   options=df['Activity'].unique(),
                                   default=None)
 
@@ -90,15 +90,15 @@ total_hours = round((selection_query['Duration'].sum()),2)
 
 st.markdown("### Employee, Customer, Activity:")
 if employee_filter:
-    employee_filter1 = employee_filter[0:]
+    employee_filter1 = employee_filter
 else:
     employee_filter1 = "All Employees"
 if customer_filter:
-    customer_filter1 = customer_filter[0:]
+    customer_filter1 = customer_filter
 else:
     customer_filter1 = "All Customers"
 if activity_filter:
-    activity_filter1 = activity_filter[0:]
+    activity_filter1 = activity_filter
 else:
     activity_filter1 = "All Activities"
     
